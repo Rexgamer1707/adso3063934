3e36feb632bec441c16f2973b9c16c78c9bd051a
Rexgamer1707 <juanjosv32@gmail.com>
2025-07-10 14:55:59 -0500

    new files and clone repository

--- 09-jQuery/js/app.js
+++ 09-jQuery/js/app.js
@@ -1,43 +1,42 @@
-$(function(){
-    if(localStorage.getItem('todolist') != null){
+$(function() {
+ 
+    // Check localStorage
+    if (localStorage.getItem('todolist') !=  null) {
         $('.list').html(localStorage.getItem('todolist'))
         countTasks()
         countRemains()
-
-    }else{
-        //count Task & Remains
+    } else {
+        // Count Tasks & Remains
         countTasks()
         countRemains()
     }
-})
-
-
-//Count Task & Remains
-//add Task
-$(function () {
-    $('footer').on('click', '#add', function () {
-        if ($('#input-task').val().length > 0) {
-
-            $task = ' <article> \
-                <input type="checkbox"> \
-                <p>'+ $('#input-task').val() + '</p> \
-                <button>&times;</button> \
-            </article>'
-            $('section.list').append($task)
+ 
+    // Add Task
+    $('footer').on('click', '#add', function() {
+        if($('#input-task').val().length > 0) {
+ 
+            $task = '<article> \
+                        <input type="checkbox"> \
+                        <p>'+$('#input-task').val()+'</p> \
+                        <button>&times;</button> \
+                     </article>'
+            $('section.list').append($task)         
             $('#input-task').val('')
             countTasks()
             countRemains()
         } else {
-            alert('please! Enter a task!')
+            alert('Please! Enter a Task')
         }
     })
-    //Toggle Task (Remain/Done)
-
-    //toggle task
-
-
-    $('body').on('click', 'input[type=checkbox]', function () {
-        //if checked
+    // Reset Tasks
+    $('body').on('click', '#reset', function() {
+        localStorage.setItem('todolist', $('.list').html(''))
+        countTasks()
+        countRemains()
+    })
+    // Toggle Task (Remain/Done)
+    $('body').on('click', 'input[type=checkbox]', function() {
+        // If checked
         if ($(this).prop('checked')) {
             $(this).attr('checked', true)
             $(this).parent().addClass('checked')
@@ -47,38 +46,24 @@ $(function () {
         }
         countRemains()
     })
-
-    //Remove Task
-    $('body').on('click', 'article button', function(){
+    // Remove Task
+    $('body').on('click', 'article button', function() {
         $(this).closest('article').remove()
         countTasks()
         countRemains()
     })
-
+ 
 })
-//Count Tasks
+// Count Tasks
 function countTasks() {
     $('.num-tasks').text($('article').length)
-    $('title-tasks').text($('article').length >1?'Tasks':'Task')
+    $('.title-tasks').text($('article').length >1?'Tasks':'Task')
 }
-
-//Count Remains
+// Count Remains
 function countRemains() {
     $remain = Math.abs($('.checked').length - $('article').length)
     $('.num-remains').text($remain)
     $('.title-remains').text($remain >1?'Remains':'Remain')
-    //Set localStorage
-    localStorage.setItem('todolist',$('.list').html())
+    // Set localStorage
+    localStorage.setItem('todolist', $('.list').html())
 }
-
-function clearList() {
-    $('.list').html('') // Vaciar la lista
-    localStorage.removeItem('todolist') // Eliminar del almacenamiento local
-    countTasks() // Recalcular tareas
-    countRemains() // Recalcular tareas pendientes
-}
-
-// Agregar evento al botón de limpiar
-$('footer').on('click', '#clear', function () {
-    clearList()
-})
